8875ba96e6644fa5f191557d694ecf804b0b9661
helen.zhou <helen.zhou1@nhs.net>
2021-11-11 12:10:10 +0000

    PRMT-1768 [HZ/KV] Add JSON structured logging

--- src/prmexporter/pipeline/main.py
+++ src/prmexporter/pipeline/main.py
@@ -1,7 +1,20 @@
 import logging
 
+from prmexporter.utils.io.json_formatter import JsonFormatter
+
+logger = logging.getLogger("prmcalculator")
+
+
+def _setup_logger():
+    logger.setLevel(logging.INFO)
+    formatter = JsonFormatter()
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
 
 def main() -> int:
+    _setup_logger()
     logging.info("Hello world")
     return 1
 
