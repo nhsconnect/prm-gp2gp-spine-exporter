550f1379b391fad4be2029855d5f600d5f850b56
helen.zhou <helen.zhou1@nhs.net>
2021-12-13 11:58:24 +0000

    PRMT-2421 [HZ] Remove redundent buffer write code

--- src/prmexporter/main.py
+++ src/prmexporter/main.py
@@ -35,15 +35,11 @@ def main():
     http_client = HttpClient(url=config.splunk_url)
     api_response_content = http_client.fetch_data(auth_token=splunk_api_token)
 
-    s3_client = boto3.resource("s3")
-    s3_spine_output_data_bucket = s3_client.Bucket(config.output_spine_data_bucket)
-
-    buffer = io.BytesIO()
-    buffer.write(api_response_content)
-    buffer.seek(0)
+    s3 = boto3.resource("s3")
+    s3_spine_output_data_bucket = s3.Bucket(name=config.output_spine_data_bucket)
 
     s3_spine_output_data_bucket.upload_fileobj(
-        api_response_content, f"{VERSION}/test-spine-data.csv"
+        io.BytesIO(api_response_content), f"{VERSION}/test-spine-data.csv"
     )
 
 
