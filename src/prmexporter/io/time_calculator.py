2ae9ddb7412ec86bed9f5a91e38e9eb69a87b4d9
Dorota-TW <dorota.sobkow1@nhs.net>
2021-12-15 17:16:45 +0000

    PRMT-2421 [DS] Optimize imports in TimeCalculator

--- src/prmexporter/io/time_calculator.py
+++ src/prmexporter/io/time_calculator.py
@@ -1,5 +1,7 @@
-import datetime
 import time
+from datetime import date, datetime
+from datetime import time as datetime_time
+from datetime import timedelta
 
 
 def _to_unix_timestamp(a_datetime):
@@ -8,15 +10,15 @@ def _to_unix_timestamp(a_datetime):
 
 class TimeCalculator:
     def __init__(self):
-        self._today = datetime.date.today()
+        self._today = date.today()
         self._today_midnight = self._get_today_midnight()
         self._yesterday_midnight = self._get_yesterday_midnight()
 
     def _get_today_midnight(self):
-        return datetime.datetime.combine(self._today, datetime.time.min)
+        return datetime.combine(self._today, datetime_time.min)
 
     def _get_yesterday_midnight(self):
-        return self._today_midnight - datetime.timedelta(days=1)
+        return self._today_midnight - timedelta(days=1)
 
     def get_today_midnight_unix_timestamp(self):
         return _to_unix_timestamp(self._today_midnight)
