9ecf574b1aec9bff39e9a869c846dfe920c00c84
Alina Balusescu <alina.balusescu1@nhs.net>
2021-12-16 10:32:13 +0000

    PRMT-2421 [DS/AB] Add more test coverage for calculating times

--- tests/unit/io/test_time_calculator.py
+++ tests/unit/io/test_time_calculator.py
@@ -25,3 +25,25 @@ def test_returns_yesterday_midnight():
     expected_yesterday_midnight = 1636675200
 
     assert actual_yesterday_midnight == expected_yesterday_midnight
+
+
+@freeze_time(datetime(year=2021, month=11, day=13, hour=0, second=0))
+def test_returns_today_midnight_when_at_midnight():
+    time_calculator = TimeCalculator()
+
+    actual_today_midnight = time_calculator.get_today_midnight_unix_timestamp()
+
+    expected_today_midnight = 1636761600
+
+    assert actual_today_midnight == expected_today_midnight
+
+
+@freeze_time(datetime(year=2021, month=1, day=1, hour=2, second=0))
+def test_returns_yesterday_midnight_when_changing_years():
+    time_calculator = TimeCalculator()
+
+    actual_yesterday_midnight = time_calculator.get_yesterday_midnight_unix_timestamp()
+
+    expected_yesterday_midnight = 1609372800
+
+    assert actual_yesterday_midnight == expected_yesterday_midnight
